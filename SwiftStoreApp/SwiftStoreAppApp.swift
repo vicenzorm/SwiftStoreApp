0b04f32793a56a4615c5d5c8419df9331c5b5952
vicenzorm <vimasera11@gmail.com>
2025-08-13 15:31:58 -0300

    First commit

---
+++ SwiftStoreApp/SwiftStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStoreAppApp.swift
+//  SwiftStoreApp
+//
+//  Created by Vicenzo Másera on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
