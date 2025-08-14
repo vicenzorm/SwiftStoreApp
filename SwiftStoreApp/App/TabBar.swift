c9b1f037bb62c706ad4cfc475352b2735a17c4a1
vicenzorm <vimasera11@gmail.com>
2025-08-14 15:37:12 -0300

    Encerramento dos primeiros passos

--- SwiftStoreApp/App/TabBar.swift
+++ SwiftStoreApp/App/TabBar.swift
@@ -12,27 +12,27 @@ struct TabBar: View {
         TabView {
             Tab("Home", systemImage: "house.fill") {
                 NavigationStack {
-                    
+                   HomeView()
                 }
             }
             Tab("Categories", systemImage: "square.grid.2x2.fill") {
                 NavigationStack {
-                    
+                    CategoriesVIew()
                 }
             }
             Tab("Cart", systemImage: "cart.fill") {
                 NavigationStack {
-                    
+                    CartView()
                 }
             }
             Tab("Favorites", systemImage: "heart.fill") {
                 NavigationStack {
-                    
+                    FavoritesView()
                 }
             }
             Tab("Orders", systemImage: "bag.fill") {
                 NavigationStack {
-                    
+                    OrdersView()
                 }
             }
         }
