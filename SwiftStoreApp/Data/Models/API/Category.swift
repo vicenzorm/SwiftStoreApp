bccfd6503fda50e7cd3bc18b694e741b1d5ba59f
vicenzorm <vimasera11@gmail.com>
2025-08-26 15:19:00 -0300

    Adds pt-BR translations for category names
    Adds Brazilian Portuguese translations for various category names in the app.
    
    This change ensures that the app can properly display localized category names,
    improving the user experience for Portuguese-speaking users.

--- SwiftStoreApp/Data/Models/API/Category.swift
+++ SwiftStoreApp/Data/Models/API/Category.swift
@@ -26,3 +26,35 @@ struct Category: Decodable, Identifiable {
         self.slug = slug
     }
 }
+
+extension Category {
+    var stringLocalized: String {
+        switch slug {
+        case "beauty": return String(localized: "Beauty")
+        case "fragrances": return String(localized: "Fragrances")
+        case "furniture": return String(localized: "Furniture")
+        case "groceries": return String(localized: "Groceries")
+        case "home-decoration": return String(localized: "Home Decoration")
+        case "kitchen-accessories": return String(localized: "Kitchen Accessories")
+        case "laptops": return String(localized: "Laptops")
+        case "mens-shirts": return String(localized: "Men's Shirts")
+        case "mens-shoes": return String(localized: "Men's Shoes")
+        case "mens-watches": return String(localized: "Men's Watches")
+        case "mobile-accessories": return String(localized: "Mobile Accessories")
+        case "motorcycle": return String(localized: "Motorcycle")
+        case "skin-care": return String(localized: "Skin Care")
+        case "smartphones": return String(localized: "Smartphones")
+        case "sports-accessories": return String(localized: "Sports Accessories")
+        case "sunglasses": return String(localized: "Sunglasses")
+        case "tablets": return String(localized: "Tablets")
+        case "tops": return String(localized: "Tops")
+        case "vehicle": return String(localized: "Vehicle")
+        case "womens-bags": return String(localized: "Women's Bags")
+        case "womens-dresses": return String(localized: "Women's Dresses")
+        case "womens-jewellery": return String(localized: "Women's Jewellery")
+        case "womens-shoes": return String(localized: "Women's Shoes")
+        case "womens-watches": return String(localized: "Women's Watches")
+        default: return name
+        }
+    }
+}
